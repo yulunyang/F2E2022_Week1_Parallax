31774fcd7c8b97acc5ae4c0fc6612090390ddbc6
crystal <yulunyangfrontend@gmail.com>
2022-10-31 14:11:24 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './index.css'
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin, TextPlugin } from "gsap/all";
 
+
+// don't forget to register plugins
+gsap.registerPlugin(gsap, ScrollTrigger, Draggable, MotionPathPlugin, TextPlugin)
 createApp(App).use(store).use(router).mount('#app')
