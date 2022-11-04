24bb0597e61ec42dd928b2476e5e77e5b5bf6eab
crystal <yulunyangfrontend@gmail.com>
2022-11-05 00:36:25 +0800

    back

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/F2E_week1_parallax'
+  publicPath: '/F2E_Week1_Parallax/'
 })
