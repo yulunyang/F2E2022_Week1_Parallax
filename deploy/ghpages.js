dda46892eec79f567091ee084bd414c5da06d2ef
crystal <yulunyangfrontend@gmail.com>
2022-11-05 19:21:33 +0800

    test

--- deploy/ghpages.js
+++ deploy/ghpages.js
@@ -1,16 +1,15 @@
 const path = require('path');
-const ghpages = require('gh-pages');
+const ghpages = require('gh-pages')
 
 const options = {
-    branch: 'gh-pages',
-    repo: 'https://github.com/yulunyang/F2E_Week1_Parallax.git'
-};
+  branch: 'gh-pages',
+  repo: 'https://github.com/yulunyang/F2E_Week1_Parallax.git'
+}
 
 const callback = err => {
-
-    if (err) console.error(err);
-    else console.log('publish success');
+  if (err) console.error(err);
+  else console.log('publish success')
 };
 
 
-ghpages.publish(path.resolve(__dirname, '../dist'), options, callback);
+ghpages.publish(path.resolve(__dirname, '../dist'), options, callback)
